285851a67c481c627a9b40190e7253955af2117e
Vinicius Silva <80004880+vinisp@users.noreply.github.com>
2022-01-25 12:15:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const $navBarItems = document.querySelectorAll(".menuItem");
+const $navBar = document.querySelector(".navBar");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 80) {
+    for (let i = 0; i < $navBarItems.length; i++) {
+      $navBarItems[i].style.opacity = "0.2";
+    }
+    $navBar.style.height = "40px";
+  } else {
+    for (let i = 0; i < $navBarItems.length; i++) {
+      $navBarItems[i].style.opacity = "1";
+    }
+    $navBar.style.height = "80px";
+  }
+}
+
+console.log($navBar);
